28d9b95fa938393abfd33832c9eb0933847e6bc6
vigilanteX <dixitaman8@gmail.com>
2025-07-12 12:02:46 +0530

    Inner class intro added

---
+++ src/main/java/org/example/Intro/Demo1.java
@@ -0,0 +1,28 @@
+package org.example.Intro;
+
+public class Demo1
+{
+    public class Child
+    {
+        public void greet()
+        {
+            System.out.println("Hello!");
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+        * Accessing from Outer class static method
+        * */
+        Demo1 demo1=new Demo1();
+        Demo1.Child child=demo1.new Child();
+        child.greet();
+
+        //Another way
+        Demo1.Child child2=new Demo1().new Child();
+        child2.greet();
+
+        //Another way
+        new Demo1().new Child().greet();
+    }
+}
